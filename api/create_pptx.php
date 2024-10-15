2436755e36aaa60af32c55c396c9d895f8fb0d9e
Ubuntu <sysadmin@nginx.avi1345mjalu1jopiwfgizz41f.zx.internal.cloudapp.net>
2024-10-15 14:20:50 +0000

    Initial demo

---
+++ api/create_pptx.php
@@ -0,0 +1,42 @@
+<?php
+$SkipCSS = true;
+require 'scripts/inc/inc.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require 'csp-functions.php';
+}
+
+$Rand = rand();
+
+use Label305\PptxExtractor\Basic\BasicExtractor;
+use Label305\PptxExtractor\Basic\BasicInjector;
+
+$extractor = new BasicExtractor();
+$mapping = $extractor->extractStringsAndCreateMappingFile(
+    '../files/template-sept-24.pptx',
+    '../files/reports/report-'.$Rand.'-extracted.pptx'
+);
+
+// Customer Name
+// Get Customer Name from API
+$AccountInfo = QueryCSP("get","v2/current_user/accounts");
+$AccountName = $AccountInfo->results[0]->name;
+// Inject Customer Name into PPTX
+$TAG_CustomerName = array_search('#TAG1',$mapping);
+$mapping[$TAG_CustomerName] = $AccountName;
+
+$injector = new BasicInjector();
+$injector->injectMappingAndCreateNewFile(
+    $mapping,
+    '../files/reports/report-'.$Rand.'-extracted.pptx',
+    '../files/reports/report-'.$Rand.'.pptx'
+);
+
+## Generate Response
+$response = array(
+    'Status' => 'Success',
+    'Path' => '/files/reports/report-'.$Rand.'.pptx'
+);
+$responseJSON = json_encode($response);
+header('Content-Type: application/json; charset=utf-8');
+echo $responseJSON;
