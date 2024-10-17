b97ee6ec8e174c8e0b45325449aeab6337bbe22c
TehMuffinMoo <mat.cox@tmmn.uk>
2024-10-17 09:46:05 +0000

    Return 0 if no results from api

--- scripts/functions/general-functions.php
+++ scripts/functions/general-functions.php
@@ -1,16 +1,20 @@
 <?php
 function number_abbr($number)
 {
-    $abbrevs = [12 => 'T', 9 => 'B', 6 => 'M', 3 => 'K', 0 => ''];
+    if (isset($number)) {
+        $abbrevs = [12 => 'T', 9 => 'B', 6 => 'M', 3 => 'K', 0 => ''];
 
-    foreach ($abbrevs as $exponent => $abbrev) {
-        if (abs($number) >= pow(10, $exponent)) {
-            $display = $number / pow(10, $exponent);
-            $decimals = ($exponent >= 3 && round($display) < 100) ? 1 : 0;
-            $number = number_format($display, $decimals).$abbrev;
-            break;
+        foreach ($abbrevs as $exponent => $abbrev) {
+            if (abs($number) >= pow(10, $exponent)) {
+                $display = $number / pow(10, $exponent);
+                $decimals = ($exponent >= 3 && round($display) < 100) ? 1 : 0;
+                $number = number_format($display, $decimals).$abbrev;
+                break;
+            }
         }
+    
+        return $number;
+    } else {
+        return 0;
     }
-
-    return $number;
 }
