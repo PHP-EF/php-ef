44d3d3904cab032f83dc0021ed104e5f418b432f
TehMuffinMoo <mat.cox@tmmn.uk>
2025-01-23 15:13:06 +0000

    dashboard fixes/improvements

--- pages/core/dashboard.php
+++ pages/core/dashboard.php
@@ -1,36 +1,44 @@
 <?php
 $Dashboards = $phpef->dashboard->getDashboards();
+$DashboardCount = count($Dashboards);
 
-$TabList = '<ul class="nav nav-tabs mb-2" role="tablist" style="margin-top: -20px;">';
-$TabContent = '<div class="tab-content">';
+if ($DashboardCount == 1) {
+    $DashboardKey = array_keys($Dashboards)[0];
+    $Content = '<div class="container-fluid mt--20">';
+    $Content .= $phpef->dashboard->buildDashboard($DashboardKey);
+    $Content .= '</div>';
+} else {
+    $TabList = '<ul class="nav nav-tabs mb-2" role="tablist" style="margin-top: -20px;">';
+    $TabContent = '<div class="tab-content">';
 
-$DashboardCount = 0;
-foreach ($Dashboards as $DashboardKey => $DashboardVal) {
-    if ($DashboardCount == 0) {
-        $active = ' active';
-    } else {
-        $active = '';
-    }
-    $TabList .= '
-    <li class="nav-item">
-        <a class="nav-link'.$active.'" id="'.$DashboardKey.'-tab" data-bs-toggle="tab" href="#'.$DashboardKey.'" role="tab" aria-controls="'.$DashboardKey.'" aria-selected="true">'.$DashboardKey.'</a>
-    </li>
-    ';
+    $DashboardBuiltCount = 0;
+    foreach ($Dashboards as $DashboardKey => $DashboardVal) {
+        if ($DashboardBuiltCount == 0) {
+            $active = ' active';
+        } else {
+            $active = '';
+        }
+        $TabList .= '
+        <li class="nav-item">
+            <a class="nav-link'.$active.'" id="'.$DashboardKey.'-tab" data-bs-toggle="tab" href="#'.$DashboardKey.'" role="tab" aria-controls="'.$DashboardKey.'" aria-selected="true">'.$DashboardKey.'</a>
+        </li>
+        ';
 
-    $TabContent .= '
-        <div class="tab-pane'.$active.'" id="'.$DashboardKey.'" role="tabpanel" aria-labelledby="'.$DashboardKey.'-tab">
-            <div class="row grid">
-                '.$phpef->dashboard->buildDashboard($DashboardKey).'
+        $TabContent .= '
+            <div class="tab-pane'.$active.'" id="'.$DashboardKey.'" role="tabpanel" aria-labelledby="'.$DashboardKey.'-tab">
+                <div class="row grid">
+                    '.$phpef->dashboard->buildDashboard($DashboardKey).'
+                </div>
             </div>
-        </div>
-    ';
+        ';
 
-    $DashboardCount++;
+        $DashboardBuiltCount++;
+    }
+
+    $TabList .= '</ul>';
+    $TabContent .= '</div>';
+    $Content = $TabList . $TabContent;
 }
-
-$TabList .= '</ul>';
-$TabContent .= '</div>';
-$Content = $TabList . $TabContent;
 
 $Content .= '
 <script>
